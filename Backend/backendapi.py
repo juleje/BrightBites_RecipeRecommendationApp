8cf35831b50af83ad3e2a3ff132482943b8d81e9
Wout Bergmans <wout.bergmans@student.kuleuven.be>
2024-12-12 22:37:52 +0100

    we do not really need this commit

--- Backend/backendapi.py
+++ Backend/backendapi.py
@@ -1,23 +1,29 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from recipe_recommender import input_query
 
 app = Flask(__name__)
-CORS(app) #Prod: CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+# Prod: CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
+CORS(app)
 
 # Example data
 tasks = [
-    {"hello":"world"}
+    {"hello": "world"}
 ]
 
 # Home route
+
+
 @app.route("/", methods=["GET"])
 def home():
     return jsonify(tasks)
 
 # Generate recipes
+
+
 @app.route("/generate", methods=["POST"])
 def generate():
-    if not request.json or "hello" not in request.json: #body valid
+    if not request.json or "hello" not in request.json:  # body valid
         print("error invalid data")
         return jsonify({"error": "Invalid data"}), 400
     data = request.json
@@ -25,8 +31,10 @@ def generate():
         "hello": data["hello"]
     }
     print(new_task)
+    input_query("Italian and tomato's")
 
     return jsonify(new_task), 201
+
 
 if __name__ == "__main__":
     app.run(debug=True)
