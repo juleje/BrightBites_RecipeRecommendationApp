0c02d1807405e4f7a5613752bd4a944dfce1c2a6
stp <quentin.stpierr@student.kuleuven.be>
2024-12-21 18:21:43 +0100

    recipes without explanation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import RecipesWithExplanation from "./components/RecipesWithExplanation";
 import RecipesWithoutExplanation from "./components/RecipesWithoutExplanation";
 import Recipe from "./components/Recipe";
+import RecipeWithoutExplanation from "./components/RecipeWithoutExplanation";
 import "./css/App.css";
 import Querymaker from "./components/Querymaker";
 import { RecipeProvider } from "./hooks/RecipeContext";
@@ -20,10 +21,10 @@ function App() {
             <Route path="/recipies" element={<RecipesWithExplanation />} />
             <Route path="/recipe/:index" element={<Recipe />} />
             <Route
-              path="/recipiesnoexplanation"
+              path="/recipesnoexplanation"
               element={<RecipesWithoutExplanation />}
             />
-            <Route path="/recipenoexplanation/:index" element={<Recipe />} />
+            <Route path="/recipenoexplanation/:index" element={<RecipeWithoutExplanation />} />
           </Routes>
         </div>
       </BrowserRouter>
